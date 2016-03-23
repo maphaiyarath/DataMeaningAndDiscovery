ad27caeed2f86129aa52534e5717353ed3223bcd
Michelle Aphaiyarath <maphaiyarath@gmail.com>
2016-03-22 23:39:39 -0500

    Enabled user to add new genre, artist, & album names.

--- project4/project4.rb
+++ project4/project4.rb
@@ -4,11 +4,16 @@
 require 'sqlite3'
 
 DB_FILE_NAME = "songs.sqlite3.db"
+SQL_SCHEMA = "SELECT songs.name, albums.name, artists.name, genres.name FROM songs, albums, artists, genres;"
+# WHERE songs.id = albums.song_id = artists.song_id = genres.song_id;"
 SQL_SELECT_GENRES = "SELECT name FROM genres;"
+SQL_SELECT_ALBUMS = "SELECT name FROM albums;"
+SQL_SELECT_ARTISTS = "SELECT name FROM artists;"
 
 # correctly configures, connects & disconnects from DB
 db = SQLite3::Database.new(DB_FILE_NAME)
 
+# displays initial menu of 5 options to user
 puts "Welcome to the music database!"
 puts "	1. Display all song information."
 puts "	2. Add a new genre."
@@ -20,18 +25,40 @@ user_input = gets.chomp
 
 case user_input
 when '1.'
-	#
+	# how to do id stuff so u can correctly print info???
+	db.execute(SQL_SCHEMA) do |row|
+		puts row
+	end
 when '2.'
+	# enables user to add new genre name
 	puts "Genres in the database:"
 	db.execute(SQL_SELECT_GENRES) do |row|
 		puts row
 	end
 	print "New genre name: "
 	new_genre = gets.chomp
+	genres_table = "INSERT INTO genres (name) VALUES ('#{new_genre}');"
+	db.execute(genres_table)
 when '3.'
-	#
+	# enables user to add new artist name
+	puts "Albums in the database:"
+	db.execute(SQL_SELECT_ALBUMS) do |row|
+		puts row
+	end
+	print "New album name: "
+	new_album = gets.chomp
+	albums_table = "INSERT INTO albums (name) VALUES ('#{new_album}');"
+	db.execute(albums_table)
 when '4.'
-	#
+	# enables user to add new album name
+	puts "Artists in the database:"
+	db.execute(SQL_SELECT_ARTISTS) do |row|
+		puts row
+	end
+	print "New artist name: "
+	new_artist = gets.chomp
+	artists_table = "INSERT INTO artists (name) VALUES ('#{new_artist}');"
+	db.execute(artists_table)
 when '5.'
 	#
 else
@@ -40,10 +67,6 @@ end
 
 db.close
 
-# displays initial menu of 5 options to user
 # lists all songs, albums, artists & genres
-# enables user to add new genre name
-# enables user to add new artist name
-# enables user to add new album name
 # enables user to add new song, per spec
 # leverages appropriate OOP
